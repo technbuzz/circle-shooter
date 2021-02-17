94706cdcdb63897922095809f5a2aef515c7641f
Samiullah Khan <samiullah.khan.1989@gmail.com>
2021-02-17 23:18:07 +0400

    added enemy

---
+++ enemy.js
@@ -0,0 +1,40 @@
+class Enemy {
+  /**
+   * 
+   * @param {number} x The x-axis
+   * @param {number} y The y-axis
+   * @param {number} radius 
+   * @param {string} color 
+   * @param {CanvasRenderingContext2D} ctx 
+   * @param {velocity} color 
+   */
+  constructor(x, y, radius, color, ctx, velocity) {
+    this._x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.ctx = ctx
+    this.velocity = velocity
+  }
+
+  set x(v) {
+    this._x = v
+  }
+
+  set y(v) {
+    this._y = v
+  }
+
+  draw () {
+    this.ctx.beginPath()
+    this.ctx.arc(this._x, this._y, this.radius, 0, Math.PI * 2, false)
+    this.ctx.fillStyle = this.color
+    this.ctx.fill()
+  }
+
+  update () {
+    this.draw()
+    this._x = this._x + this.velocity.x
+    this._y = this._y + this.velocity.y
+  }
+}
--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
+canvas.width = 800
+canvas.height = 600
+
 const center = {
-  x: innerWidth/2,
-  y: innerWidth/2
+  x: canvas.width/2,
+  y: canvas.height/2
 }
 
-canvas.width = innerWidth
-canvas.height = innerHeight
 
 const player = new Player(100, 100, 30, 'blue', ctx)
 player.x = center.x
@@ -17,6 +18,36 @@ player.y = center.y
 player.draw()
 
 let projectiles = []
+let enemies = []
+
+function spawnEnemies() {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 4) + 4
+    let x
+    let y
+
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+      y = Math.random() * canvas.height
+    } else {
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+
+    }
+
+
+    const color = 'green'
+    const angle = Math.atan2(center.y - y, center.x - x)
+
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle)
+    }
+
+    const enemy = new Enemy(x, y, radius, color, ctx, velocity)
+    enemies.push(enemy)
+  }, 1000);
+}
 
 addEventListener('click', ({ clientX, clientY }) => {
 	const angle = Math.atan2(clientY - canvas.height/2, clientX - canvas.width/2)
@@ -32,10 +63,17 @@ addEventListener('click', ({ clientX, clientY }) => {
 })
 
 function animate () {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  player.draw()
   projectiles.forEach(p => {
     p.update()
+  })
+  enemies.forEach(e => {
+    e.update()
   })
   requestAnimationFrame(animate)
 }
 
 animate()
+spawnEnemies()
